7da0e9cf098a6b5264e8be4bcb4adcc48ba55cd8
honey john <131943776+honey39@users.noreply.github.com>
2023-12-11 14:44:52 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ttutorial in Data Science using ecommerce data',
+    author='honeyjohn',
+    license='MIT',
+)
